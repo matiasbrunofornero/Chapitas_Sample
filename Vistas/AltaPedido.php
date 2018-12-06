1a44fc81e362238fae860ff3cd295752851e6cdd
Matias Bruno <matias.bruno@globant.com>
2018-12-06 09:32:10 -0300

    AltaPedido.php edited: Input to charge size added

--- Vistas/AltaPedido.php
+++ Vistas/AltaPedido.php
@@ -3,13 +3,21 @@
 	<div class="container" style="padding: 150px">
 	    <div class="row">
 	        <div>
-	            <div id="inicio">
+                <h1> Alta Pedido </h1>
+                <form role= "form" method="POST" action="<?php echo DIR . 'pedido/ingresarPedido'?>">
+	                <div class= "row">
+                        <div class= "form-group">
+                            <label>Tamano</label>
+                            <input type="text" name="tamano" id="tamano" required placeholder="Ingrese el tamano">
+                        </div>
+                    </div>
 
-	                <input type="submit" value="FINALIZAR CARGA" 
-					style="width: 230px" onclick="location=''">
-					<br><br>
 
-	            </div>
+                        <input type="submit" value="FINALIZAR CARGA" 
+                        style="width: 230px" onclick="location=''">
+                        <br><br>
+
+	                </div>
 	        </div>
 	    </div>
 	</div>
