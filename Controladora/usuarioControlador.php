bb246479fbc4e9ac0abde368f029e2e31aff5ba6
Matias <matiasbrunofornero@gmail.com>
2018-12-07 23:20:38 -0300

    UsuarioControlador.php has been created correctly

---
+++ Controladora/usuarioControlador.php
@@ -0,0 +1,45 @@
+<?php namespace Controladora;
+
+use DAO\usuarioDAO as UsuarioDAO;
+use Modelo\Usuario as Usuario;
+
+class usuarioControlador
+{
+    private $usuarioDAO;
+
+    public function __construct()
+    {
+        $this->usuarioDAO = UsuarioDAO::getInstance();
+    }
+
+    public function buscarUsuario($email, $password)
+    {
+        try
+        {
+            if($this->usuarioDAO->buscar($email, $password)!=null)
+            {
+                $message = "Login exitoso. Bienvenido a ChapitasPHP";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+                
+                require_once 'Vistas/InicioIndex.php';
+            }
+            else
+            {
+                $message = "Datos incorrectos. Intente nuevamente";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+
+                require_once 'Vistas/Inicio.php';
+            }
+        }
+        catch(\Exception $e)
+        {
+            $message = $e->getMessage();
+            echo "<script type='text/javascript'>alert('$message');</script>";
+
+            require_once 'Vistas/Inicio.php';
+        }
+    }
+}
+
+
+
