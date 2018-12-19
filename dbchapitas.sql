229097b82f247ce03cc714f8455a786ee0a87114
Matias <matiasbrunofornero@gmail.com>
2018-12-19 00:29:25 -0300

    dbchapitas.sql updated

--- dbchapitas.sql
+++ dbchapitas.sql
@@ -2,10 +2,10 @@
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 13, 2018 at 11:28 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.2.12
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-12-2018 a las 04:27:32
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,55 +19,61 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dbchapitas`
+-- Base de datos: `dbchapitas`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `chapitas`
+-- Estructura de tabla para la tabla `chapitas`
 --
 
 CREATE TABLE `chapitas` (
-  `Numero` int(11) NOT NULL,
+  `Numero` int(20) NOT NULL,
   `Tamano` varchar(20) NOT NULL,
-  `Nombre` varchar(50) NOT NULL,
+  `Mascota` varchar(20) NOT NULL,
   `Telefono` varchar(20) NOT NULL,
-  `Cliente` varchar(100) NOT NULL,
-  `Petshop` varchar(100) NOT NULL
+  `Cliente` varchar(20) NOT NULL,
+  `Petshop` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `chapitas`
+-- Volcado de datos para la tabla `chapitas`
 --
 
-INSERT INTO `chapitas` (`Numero`, `Tamano`, `Nombre`, `Telefono`, `Cliente`, `Petshop`) VALUES
-(1, 'Grande', 'Renata', '2236034246', 'Magali Fernandez', 'A La Cucha'),
-(2, 'Grande', 'Dorita', '2236697977', 'Matias Bruno', 'A La Cucha');
+INSERT INTO `chapitas` (`Numero`, `Tamano`, `Mascota`, `Telefono`, `Cliente`, `Petshop`) VALUES
+(1, 'Gatito', 'Limona', '2236049503', 'Stella', '4'),
+(4, 'Grande', 'Luna', '2235258000', 'Maria Rosa', '3'),
+(8, 'Gatito', 'Peluda', '2234752228', 'Florencia', '1');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `petshops`
+-- Estructura de tabla para la tabla `petshops`
 --
 
 CREATE TABLE `petshops` (
-  `Numero` varchar(30) NOT NULL,
-  `Nombre` varchar(30) NOT NULL,
-  `Direccion` varchar(50) NOT NULL
+  `Numero` int(11) NOT NULL,
+  `Nombre` varchar(40) NOT NULL,
+  `Direccion` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `petshops`
+-- Volcado de datos para la tabla `petshops`
 --
 
 INSERT INTO `petshops` (`Numero`, `Nombre`, `Direccion`) VALUES
-('1', 'A La Cucha', 'Cordoba 3100');
+(1, 'Veterinaria Libra', 'Santa Fe 2313'),
+(2, 'Veterinaria Paso', 'Av. Paso 3102'),
+(3, 'Veterinaria Sol', 'Av. Tejedor 2628'),
+(4, 'Royal Pet', 'Libertad 4800'),
+(5, 'Pet Mariela', 'Strobel 5700'),
+(6, 'Pet Fantasma', 'Diag. EE UU 889');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -77,45 +83,57 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `usuarios`
+-- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`UsuarioId`, `Email`, `Password`) VALUES
-(1, 'admin@admin.com', 'administrador');
+(1, 'administrador', 'administrador'),
+(2, 'admin@admin.com', '123456'),
+(3, 'magaliten', '36616668');
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `chapitas`
+-- Indices de la tabla `chapitas`
 --
 ALTER TABLE `chapitas`
-  ADD PRIMARY KEY (`Numero`),
-  ADD UNIQUE KEY `NUMERO_UNIQUE` (`Numero`);
+  ADD PRIMARY KEY (`Numero`);
 
 --
--- Indexes for table `usuarios`
+-- Indices de la tabla `petshops`
+--
+ALTER TABLE `petshops`
+  ADD PRIMARY KEY (`Numero`);
+
+--
+-- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`UsuarioId`),
-  ADD UNIQUE KEY `Password_UNIQUE` (`Password`);
+  ADD PRIMARY KEY (`UsuarioId`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `chapitas`
+-- AUTO_INCREMENT de la tabla `chapitas`
 --
 ALTER TABLE `chapitas`
-  MODIFY `Numero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Numero` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `usuarios`
+-- AUTO_INCREMENT de la tabla `petshops`
+--
+ALTER TABLE `petshops`
+  MODIFY `Numero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
